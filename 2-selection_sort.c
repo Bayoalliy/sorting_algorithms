6cdfe0ef8abf05b055ed7537029191afce06a44a
bayoalliy <adebayoalliy@gmail.com>
2023-11-20 08:52:45 -0800

    sorted

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,22 +10,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t smallest, i, j, tmp, smallest_ind;
+	size_t i, j, tmp, smallest_ind;
+
+	int smallest, swap;
 
 	for (i = 0; i < size; i++)
 	{
+		swap = 0;
 		smallest = array[i];
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] < smallest)
 			{
 				smallest = array[j];
 				smallest_ind = j;
+				swap = 1;
 			}
 		}
-		tmp = array[i];
-		array[i] = smallest;
-		array[smallest_ind] = tmp;
-		print_array(array, size);
+		if (swap)
+		{
+			tmp = array[i];
+			array[i] = smallest;
+			array[smallest_ind] = tmp;
+			print_array(array, size);
+		}
 	}
 }
